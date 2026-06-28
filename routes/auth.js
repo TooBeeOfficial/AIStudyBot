d510c587344f09908e887784f5928bbb4a60f101
Ersan Tzampaz Chatip <hatipersan@gmail.com>
2026-06-28 06:31:34 +0300

    changed to temporary https.

--- routes/auth.js
+++ routes/auth.js
@@ -99,7 +99,7 @@ router.get(
       email: user.email,
       name: user.name,
     };
-    res.redirect("http://localhost:4200");
+    res.redirect("https://upset-schools-love.loca.lt");
   },
 );
 
